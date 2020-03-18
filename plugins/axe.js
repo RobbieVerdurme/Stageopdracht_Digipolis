26beeace248834fabddbe182796782dc2f01af6d
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-18 12:39:59 +0100

    [Added] axe and cypress

---
+++ plugins/axe.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueAxe from 'vue-axe'
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== 'production') {
+    Vue.use(VueAxe, {
+        config: {
+            disableOtherRules: true,
+            reporter: 'no-passes',
+            rules: [
+                { id: 'heading-order', enabled: true },
+                { id: 'label-title-only', enabled: true },
+                { id: 'link-in-text-block', enabled: true },
+                { id: 'button-name', enabled: true},
+                { id: 'skip-link', enabled: true },
+                { id: 'help-same-as-label', enabled: true },
+                {id: 'dlitem', enabled: true},
+                {id: 'definition-list', enabled: true}
+            ]
+        }
+    }
+    )
+}
