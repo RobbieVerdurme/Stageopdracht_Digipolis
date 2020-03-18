4df3879a40bd028e6480ce28892c7c1920c53a52
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-18 15:47:26 +0100

    [Add] overview test

---
+++ tests/e2e/integration/overview.spec.js
@@ -0,0 +1,88 @@
+/* eslint-disable */
+// the things that axe has to check
+const config = [
+    { id: 'heading-order', enabled: true },
+    { id: 'label-title-only', enabled: true },
+    { id: 'link-in-text-block', enabled: true },
+    { id: 'button-name', enabled: true },
+    { id: 'skip-link', enabled: true },
+    { id: 'help-same-as-label', enabled: true },
+    { id: 'dlitem', enabled: true },
+    { id: 'definition-list', enabled: true }
+]
+
+describe('OverviewPage', () => {
+    // do before each test
+    beforeEach(() => {
+        // TODO intercept backendcall for clusters
+
+        // navigate to overview
+        cy.visit('/overview/')
+
+        cy.injectAxe()
+    })
+
+    //test if axe gives errors
+    it('Has no detectable a11y violations on load (custom configuration)', () => {
+        // Configure aXe and test the page at initial load
+        cy.configureAxe({
+            disableOtherRules: true,
+            reporter: 'no-passes',
+            rules: config
+        })
+        cy.checkA11y()
+    })
+
+    /**
+     * LIST
+     */
+    // check if list is there
+    it('Check if Display switcher is on page', () => {
+        // check if item is visable
+        cy.contains('As list').should('be.visible')
+
+        // check if links have text
+        cy.contains('As list')
+        cy.contains('On map')
+
+        // check if link is disabled(of attribute span)
+        cy.contains('As list').should('have.class', '')
+
+        // check the first item of the list
+        // must be item of the backendcall
+    })
+
+    // test redirect to cluster
+    it('Redirect to cluster', () => {
+        // click on first readmore link
+        cy.get('.read-more').first().click()
+
+        // url check
+        cy.url().should('include', '1')
+    })
+
+    /**
+     * MAP
+     */
+    // Navigate to map
+    it('Navigate to map page', () => {
+        // Navigate to map
+        cy.contains('On map').click()
+
+        // url check
+        cy.url().should('include', '/overview/map')
+    })
+
+    // check if everything is there
+    it('Check if everything is on map page', () => {
+        // Navigate to map
+        cy.contains('On map').click()
+
+        // check if map is there
+        // This gives error 'does not find item with prop load-tiles-while-animating'
+        // cy.get('[load-tiles-while-animating]')
+
+        // cta block should be visible
+        cy.get('[download]').should('be.visible')
+    })
+})
