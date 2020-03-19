72f441295169704711a34e1054c8753dc8d0f6b5
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-19 10:20:04 +0100

    [FIX] axe test

--- components/organisms/footer.vue
+++ components/organisms/footer.vue
@@ -1,46 +1,48 @@
 <template>
-  <section class="footer">
-    <h2 class="visually-hidden">Footer</h2>
-    <div class="footer-top">
-      <div class="content-container">
+  <footer>
+    <section class="footer">
+      <h2 class="visually-hidden">Footer</h2>
+      <div class="footer-top">
+        <div class="content-container">
           <!--gent info-->
-        <div class="footer--column">
-          <gentinfo />
-        </div>
+          <div class="footer--column">
+            <gentinfo />
+          </div>
 
-        <!--links-->
-        <div class="footer--column">
+          <!--links-->
+          <div class="footer--column">
             <cityLinks />
-        </div>
+          </div>
 
-        <!--adress-->
-        <div class="footer--column">
+          <!--adress-->
+          <div class="footer--column">
             <postAdress />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="footer-bottom content-container">
+      <div class="footer-bottom content-container">
         <!--partnership-->
-      <div class="footer--column">
+        <div class="footer--column">
           <partnership />
-      </div>
+        </div>
 
-      <!--social media-->
-      <div class="footer--column">
-        <socialMedia />
+        <!--social media-->
+        <div class="footer--column">
+          <socialMedia />
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </footer>
 </template>
 
 <script>
 export default {
   components: {
-    gentinfo: () => import('~/components/molecules/gentinfo.vue'),
-    cityLinks: () => import('~/components/molecules/city-links.vue'),
-    postAdress: () => import('~/components/molecules/post-adress.vue'),
-    partnership: () => import('~/components/molecules/partnership.vue'),
-    socialMedia: () => import('~/components/molecules/social-media.vue')
+    gentinfo: () => import("~/components/molecules/gentinfo.vue"),
+    cityLinks: () => import("~/components/molecules/city-links.vue"),
+    postAdress: () => import("~/components/molecules/post-adress.vue"),
+    partnership: () => import("~/components/molecules/partnership.vue"),
+    socialMedia: () => import("~/components/molecules/social-media.vue")
   }
-}
+};
 </script>
