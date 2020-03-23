d088d22c68455f61bb8e2b5cf3df003a5f6d57cb
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-23 14:02:27 +0100

    [Add] first popup

--- components/organisms/vuelayersmap.vue
+++ components/organisms/vuelayersmap.vue
@@ -2,6 +2,60 @@
   <div>
     <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" :data-projection="dataProjection" style="max-height: 400px">
       <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" />
+      <!-- interactions -->
+      <vl-interaction-select :features.sync="selectedFeatures">
+        <template slot-scope="select">
+          <!-- select styles -->
+          <vl-style-box>
+            <vl-style-stroke color="#423e9e" :width="7"></vl-style-stroke>
+            <vl-style-fill :color="[254, 178, 76, 0.7]"></vl-style-fill>
+            <vl-style-circle :radius="5">
+              <vl-style-stroke color="#423e9e" :width="7"></vl-style-stroke>
+              <vl-style-fill :color="[254, 178, 76, 0.7]"></vl-style-fill>
+            </vl-style-circle>
+          </vl-style-box>
+          <vl-style-box :z-index="1">
+            <vl-style-stroke color="#d43f45" :width="2"></vl-style-stroke>
+            <vl-style-circle :radius="5">
+              <vl-style-stroke color="#d43f45" :width="2"></vl-style-stroke>
+            </vl-style-circle>
+          </vl-style-box>
+          <!--// select styles -->
+
+          <!-- selected feature popup -->
+          <vl-overlay class="feature-popup" v-for="feature in select.features" :key="feature.id" :id="feature.id" :position="pointOnSurface(feature.geometry)" :auto-pan="true" :auto-pan-animation="{ duration: 300 }">
+            <template slot-scope="popup">
+              <section class="card">
+                <header class="card-header">
+                  <p class="card-header-title">
+                    Feature ID {{ feature.id }}
+                  </p>
+                  <a class="card-header-icon" title="Close"
+                     @click="selectedFeatures = selectedFeatures.filter(f => f.id !== feature.id)">
+                    <b-icon icon="close"></b-icon>
+                  </a>
+                </header>
+                <div class="card-content">
+                  <div class="content">
+                    <p>
+                      Overlay popup content for Feature with ID <strong>{{ feature.id }}</strong>
+                    </p>
+                    <p>
+                      Popup: {{ JSON.stringify(popup) }}
+                    </p>
+                    <p>
+                      Feature: {{ JSON.stringify({ id: feature.id, properties: feature.properties }) }}
+                    </p>
+                  </div>
+                </div>
+              </section>
+            </template>
+          </vl-overlay>
+          <!--// selected popup -->
+        </template>
+      </vl-interaction-select>
+      <!--// interactions -->
+
       <!--Tile for map-->
       <vl-layer-tile id="wmts">
         <!--Get map from geoserver-->
@@ -52,6 +106,7 @@
 </template>
 
 <script>
+  import { findPointOnSurface } from 'vuelayers/lib/ol-ext'
 export default {
   // variables
   data () {
@@ -70,16 +125,15 @@ export default {
       center: [414688.076662, 6630700.090869],
       rotation: 0,
       geolocPosition: undefined,
-    }
-  },
-  computed: {
-    // features(POI)
-    features () {
-      return this.$store.state.poi.pointsOfInterst
-    },
-    // route
-    route () {
-      return this.$store.state.routes.routepoints
+
+      // features
+      features: this.$store.state.poi.pointsOfInterst,
+
+      // route
+      route : this.$store.state.routes.routepoints,
+
+      // popup of item
+      selectedFeatures: [],
     }
   },
   // created
@@ -99,6 +153,25 @@ export default {
     updatePosition ($event) {
       this.geolocPosition = $event
     },
+    pointOnSurface: findPointOnSurface
   }
 }
 </script>
+<style scoped>
+  .card-content{
+    max-height: 20em;
+    overflow: auto;
+  }
+
+  .feature-popup{
+    position: absolute;
+    left: -50px;
+    bottom: 12px;
+    width: 20em;
+    max-width: none;
+  }
+
+  .content{
+    word-break: break-all;
+  }      
+</style>
