c45b5834182c5603edc68dee4d6b41769ed17e5a
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-03-23 08:44:07 +0100

    [Add] command axe to cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,49 @@
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// the things that axe has to check
+const config = [
+    { tags: ['wcag2a', 'wcag2aa']},
+    { id: 'heading-order', enabled: true },
+    { id: 'label-title-only', enabled: true },
+    { id: 'link-in-text-block', enabled: true },
+    { id: 'button-name', enabled: true },
+    { id: 'skip-link', enabled: true },
+    { id: 'help-same-as-label', enabled: true },
+    { id: 'dlitem', enabled: true },
+    { id: 'definition-list', enabled: true }
+]
+
+// add axe test
+Cypress.Commands.add('axe', () => {  
+        // Configure aXe and test the page at initial load
+        cy.configureAxe({
+            reporter: 'no-passes',
+            rules: config
+        })
+        cy.checkA11y()
+  });
--- tests/e2e/integration/scanner.spec.js
+++ tests/e2e/integration/scanner.spec.js
@@ -1,17 +1,4 @@
 /* eslint-disable */
-// the things that axe has to check
-const config = [
-    { tags: ['wcag2a', 'wcag2aa']},
-    { id: 'heading-order', enabled: true },
-    { id: 'label-title-only', enabled: true },
-    { id: 'link-in-text-block', enabled: true },
-    { id: 'button-name', enabled: true },
-    { id: 'skip-link', enabled: true },
-    { id: 'help-same-as-label', enabled: true },
-    { id: 'dlitem', enabled: true },
-    { id: 'definition-list', enabled: true }
-]
-
 describe('ScannerPage', () => {
     // do before each test
     beforeEach(() => {
@@ -21,12 +8,7 @@ describe('ScannerPage', () => {
 
     //test if axe gives errors
     it('Has no detectable a11y violations on load (custom configuration)', () => {
-        // Configure aXe and test the page at initial load
-        cy.configureAxe({
-            reporter: 'no-passes',
-            rules: config
-        })
-        cy.checkA11y()
+        cy.axe()
     })
 
     //check if info is on scanner page
