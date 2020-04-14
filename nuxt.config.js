d3d0b3beac817acb3e99b83f58153d2f7b911f55
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-14 15:51:38 +0200

    [Update] place to get images

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,21 +25,21 @@ export default {
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/images/apple-touch-icon.png'
+        href: '/images/cyan/apple-touch-icon.png'
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '/images/favicon-32x32.png'
+        href: '/images/cyan/favicon-32x32.png'
       },
       {
         rel: 'mask-icon',
-        href: '/images/safari-pinned-tab.svg'
+        href: '/images/cyan/safari-pinned-tab.svg'
       },
       {
         rel: 'icon',
-        type: 'image/x-icon',
+        type: 'image/cyan/x-icon',
         href: '/favicon.ico'
       }
     ],
