61f672d42406f9ec12864ad0bae192ac8a130e4f
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-04-20 08:37:30 +0200

    [Update] feedback.vue send after call

--- components/molecules/feedback.vue
+++ components/molecules/feedback.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="box-no-icon">
     <h2>
-      <button aria-expanded="true">Kunnen we deze pagina nog verbeteren? Laat het ons weten!</button>
+      <button aria-expanded="true">
+        Kunnen we deze pagina nog verbeteren? Laat het ons weten!
+      </button>
     </h2>
     <div>
       <!--Contact-->
       <h3>Voor persoonlijke vragen of klachten kan je terecht bij Gentinfo.</h3>
-      <p class>Hoe bereiken?</p>
+      <p class>
+        Hoe bereiken?
+      </p>
       <ul class="icon-list">
         <li>
-          <i class="icon-phone"></i>
+          <i class="icon-phone" />
           <span>
             Telefonisch:
             <a href="tel: 09 210 10 10">09 210 10 10</a> (van maandag t.e.m. zaterdag van 8 tot 19 uur)
           </span>
         </li>
         <li>
-          <i class="icon-envelope"></i>
+          <i class="icon-envelope" />
           <span>
             Stuur ons een
             <a href="mailto:gentinfo@stad.gent">e-mail</a>
           </span>
         </li>
         <li>
-          <i class="icon-twitter-outline"></i>
+          <i class="icon-twitter-outline" />
           <span>
             Via de
             <a href="https://twitter.com/stadgent">Twitter-account</a> van Stad Gent (
@@ -39,7 +43,6 @@
       <div v-if="!send">
         <!--Form elements-->
         <div :class="err?'error':success?'success':''" class="form-item">
-
           <!--Label-->
           <label for="feedbacktekst">
             Bericht
@@ -51,10 +54,10 @@
             <!--Text-->
             <div class="form-item-column">
               <textarea
-                name="feedbacktekst"
                 id="feedbacktekst"
+                name="feedbacktekst"
                 :class="err?'error':success?'success':''"
-              ></textarea>
+              />
             </div>
             <!--/Text-->
 
@@ -62,20 +65,22 @@
             <div class="form-item-column">
               <div
                 v-if="err || success"
+                id="textarea-message"
                 :class="err?'error':success?'success':''"
                 class="field-message"
                 role="alert"
-                id="textarea-message"
               >
                 <span v-if="err">{{ err }}</span>
                 <span v-if="success">We hebben u suggestie ontvangen.</span>
-                <div class="accolade"></div>
+                <div class="accolade" />
               </div>
             </div>
             <!--/Status-->
           </div>
         </div>
-        <button @click="sendFeedback" class="button button-secondary">Indienen</button>
+        <button class="button button-secondary" @click="sendFeedback">
+          Indienen
+        </button>
         <!--/Form elements-->
       </div>
       <!--/Feedback-->
@@ -91,43 +96,44 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      feedbacktext: "",
+      feedbacktext: '',
       send: false,
       success: false,
-      err: ""
-    };
+      err: ''
+    }
   },
   methods: {
     /**
      * send feedback to server
      */
-    sendFeedback() {
+    sendFeedback () {
       // check if already sending
       if (!this.send) {
-        this.send = true;
+        this.send = true
         this.$store
-          .dispatch("sendFeedback", this.feedbacktext)
+          .dispatch('sendFeedback', this.feedbacktext)
           .then(() => {
             // on success backend call
-            this.success = true;
-            this.resetFeedbackText();
+            this.success = true
+            this.resetFeedbackText()
           })
-          .catch(error => {
+          .catch((error) => {
             // on error
-            this.err = error;
-          });
-        // reset send
-        this.send = false;
+            this.err = error
+          }).then(() => {
+            // reset send
+            this.send = false
+          })
       }
     },
     /**
      * resets the feedback text to ''
      */
-    resetFeedbackText() {
-      this.feedbacktext = "";
+    resetFeedbackText () {
+      this.feedbacktext = ''
     }
   }
-};
+}
 </script>
