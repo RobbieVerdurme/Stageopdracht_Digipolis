0326b13f3c87ed0ae0504bab2dbe2e8a7a44a71f
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-04 14:29:11 +0200

    [Update] cta some changes to computed and store actions

--- components/molecules/cta.vue
+++ components/molecules/cta.vue
@@ -5,11 +5,11 @@
         download website als app enkel beschikbaar op chromium browsers ...
       </p>
       <p>download gpx hieronder</p>
+
       <ul class="links">
         <li>
           <a href="#" class="button button-primary" download title="Dowload gpx lichtfestival">
             gpx Lichtfestival</a>
-
           <span class="file-type">
             gpx
             <span class="file-size">
@@ -17,10 +17,10 @@
             </span>
           </span>
         </li>
+
         <li ref="installableblock">
           <a class="standalone-link" style="cursor: pointer;" title="Download application" @click="installPWA">
             Install PWA</a>
-
           <span class="file-type">
             Application
             <span class="file-size">
@@ -32,27 +32,31 @@
     </div>
   </section>
 </template>
+
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+
 /* eslint-disable no-undef */
 export default {
   computed: {
-    installable () {
-      return this.$store.getters.getInstallable
-    },
-    installPrompt () {
-      // eslint-disable-next-line dot-notation
-      return this.$store.getters['getInstallablePrompt']
-    }
+    ...mapGetters(
+      {
+        installable: 'getInstallable',
+        installPrompt: 'getInstallablePrompt'
+      }
+    )
   },
+
   mounted () {
     this.$refs.installableblock.hidden = !this.installable
     window.addEventListener('beforeinstallprompt', (e) => {
       // Stash the event so it can be triggered later.
-      this.$store.commit('setInstallablePrompt', e)
+      this.setInstallablePrompt(e)
       // Update UI to notify the user they can add to home screen
-      this.setInstallable(true)
+      this.setNewInstallable(true)
     })
   },
+
   methods: {
     /**
      * prompt the install
@@ -62,22 +66,26 @@ export default {
       this.installPrompt.prompt()
 
       // set display to none
-      this.setInstallable(false)
+      this.setNewInstallable(false)
 
       // check what user picked
       this.installPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome !== 'accepted') {
-          this.setInstallable(true)
+          this.setNewInstallable(true)
         }
       })
     },
     /**
      * set the store installable value
      */
-    setInstallable (newValue) {
-      this.$store.commit('setInstallable', newValue)
+    setNewInstallable (newValue) {
+      this.setInstallable(newValue)
       this.$refs.installableblock.hidden = !newValue
-    }
+    },
+    ...mapMutations([
+      'setInstallable',
+      'setInstallablePrompt'
+    ])
   }
 }
 </script>
