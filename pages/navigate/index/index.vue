20cfaf7b48f2534522e7f21f890735752e67ae9d
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-04 13:26:08 +0200

    [Update] navigate/index/index show last visited poi

--- pages/navigate/index/index.vue
+++ pages/navigate/index/index.vue
@@ -2,5 +2,27 @@
   <div>
     <h1>Routering</h1>
     <p>De detail pagina van het point of interst zal hier getoont worden indien u dicht genoeg bij een punt staat</p>
+    <p v-if="lastpoiId">
+      Het laatste opgeslagen punt is <nuxt-link :to="{name: 'navigate-index-id', params: {id: lastpoiId}}">
+        {{ lastPOI.properties.naam_nl }}
+      </nuxt-link>
+    </p>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      lastpoiId: null
+    }
+  },
+  computed: {
+    lastPOI () {
+      return this.$store.getters.getPOIDetailById(this.lastpoiId)
+    }
+  },
+  mounted () {
+    this.lastpoiId = parseInt(localStorage.lastPoiId)
+  }
+}
+</script>
