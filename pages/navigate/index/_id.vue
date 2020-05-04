f6e004f28f0383e6a6bcd69dd62cc721a69f1848
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-04 13:25:35 +0200

    [Update] navigate/index/_id add save button

--- pages/navigate/index/_id.vue
+++ pages/navigate/index/_id.vue
@@ -1,5 +1,17 @@
 <template>
-  <info :item="item.properties" />
+  <div>
+    <div v-if="savedPoi" class="messages messages--status" role="alert" aria-atomic="true">
+      <h2 class="visually-hidden">
+        Status
+      </h2>
+      <i class="icon-checkmark" aria-hidden="true" />
+      <p>{{ item.properties.naam_nl }} is opgeslagen</p>
+    </div>
+    <info :item="item.properties" />
+    <button ref="saveButton" :disabled="savedPoi" class="button button-secondary" @click="savePOI">
+      Punt opslaan
+    </button>
+  </div>
 </template>
 
 <script>
@@ -11,14 +23,32 @@ export default {
   data () {
     return {
       /**
-       * id of the cluster
+       * id of the poi
        */
-      id: parseInt(this.$route.params.id)
+      id: parseInt(this.$route.params.id),
+      savedPoi: false
     }
   },
   computed: {
+    /**
+     * selected poi
+     */
     item () {
       return this.$store.getters.getPOIDetailById(this.id)
+    }
+  },
+  mounted () {
+    this.savedPoi = parseInt(localStorage.lastPoiId) === this.id
+  },
+  methods: {
+    /**
+     * save last poi id to localstorage
+     */
+    savePOI () {
+      localStorage.lastPoiId = this.id
+
+      // disable button
+      this.savedPoi = true
     }
   }
 }
