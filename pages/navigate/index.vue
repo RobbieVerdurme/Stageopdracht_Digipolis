18dcf00a743e5d3bdc3ce1467ea077f845b1d761
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-05 16:12:20 +0200

    [Update] navigate index show visited points

--- pages/navigate/index.vue
+++ pages/navigate/index.vue
@@ -1,10 +1,21 @@
 <template>
   <main class="news-overview-page">
-    <section class="overview-layout">
+    <section class="overview-layout flow-container">
       <client-only>
         <vMap :features.sync="features" :route.sync="route" class="map" @locationChanged="locationChanged" />
       </client-only>
-      <nuxt-child />
+      <nuxt-child class="left" />
+      <div v-if="visitedPoiList.length" class="right">
+        <h3>Bezochte punten</h3>
+        <collection :items="filterdVisitedPoiList" />
+        <pagination
+          :items="visitedPoiList"
+          :current-page="currentPage"
+          :limit-total-pages="pageLimit"
+          :to-page="$route.name"
+          @updateFilterdItems="updateFilterdItems"
+        />
+      </div>
     </section>
   </main>
 </template>
@@ -13,6 +24,8 @@
 export default {
   middleware: ['poi', 'route'],
   components: {
+    pagination: () => import('~/components/molecules/pagination'),
+    collection: () => import('~/components/organisms/collection'),
     vMap: () => import('~/components/organisms/vuelayersmap.vue')
   },
   /**
@@ -41,20 +54,45 @@ export default {
       route: this.$store.getters.getAllRoutes,
 
       // position
-      position: null
+      position: null,
+
+      // list of visited poi
+      visitedPoiList: [],
+      filterdVisitedPoiList: [],
+
+      // settings to configure pagination
+      currentPage: null,
+      pageLimit: 0
     }
   },
   computed: {
     visited () {
-      return localStorage.visitedPOI ? localStorage.visitedPOI : [...Array(this.features.length)].map((x) => { return false })
+      return localStorage.getItem('visitedPOI') ? JSON.parse(localStorage.getItem('visitedPOI')) : [...Array(this.features.length)].map((x) => { return false })
     }
   },
   watch: {
+    $route () {
+      this.currentPage = this.$route.query.page ? parseInt(this.$route.query.page) : 1
+    },
     /**
      * when position changed check if it is close to a poi
      */
     position () {
       this.showClosedPOI()
+    }
+  },
+  created () {
+    this.currentPage = this.$route.query.page ? parseInt(this.$route.query.page) : 1
+  },
+  mounted () {
+    if (localStorage.visitedPOI) {
+      const visited = JSON.parse(localStorage.getItem('visitedPOI'))
+      for (const index in visited) {
+        if (visited[index]) {
+          this.visitedPoiList.push(this.features[index])
+        }
+      }
+      this.pageLimit = this.visitedPoiList.length > 10 ? 3 : this.visitedPoiList.length > 5 ? 2 : 0
     }
   },
   methods: {
@@ -72,9 +110,8 @@ export default {
         // check if the poi is in range
         if (inRangeOfLongitude && inRangeOfLangitude && !this.visited[index]) {
           this.visited[index] = true
-          // change nuxt child with the id of the item
-          // this.$router.push({ name: 'navigate-index-id', params: { id: this.features[index].properties.volgnummer } })
-          // break
+          this.visitedPoiList.push(this.features[index])
+          this.pageLimit = this.visitedPoiList.length > 10 ? 3 : this.visitedPoiList.length > 5 ? 2 : 0
           this.$toast.show('Je bent in de buurt van ' + this.features[index].properties.naam_nl, {
             action: {
               text: 'Toon',
@@ -89,7 +126,8 @@ export default {
           })
         }
       }
-      localStorage.visitedPOI = this.visited
+      const parsed = JSON.stringify(this.visited)
+      localStorage.setItem('visitedPOI', parsed)
     },
     /**
      * check if value is between min and max
@@ -102,17 +140,42 @@ export default {
      */
     locationChanged (value) {
       this.position = value
+    },
+    updateFilterdItems (value) {
+      this.filterdVisitedPoiList = value
     }
   }
 }
 </script>
 <style scoped>
 .map {
-    max-height: 20em;
-}
-@media screen and (min-width: 770px) {
-  .map {
     max-height: 30em;
-  }
+}
+.flow-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-bottom: 0;
+}
+.flow-container > * {
+  margin-bottom: 2rem;
+  width: 100%;
+}
+
+.flow-container > .left {
+  position: relative;
+  font-size: .8rem;
+  min-height: 7rem;
+}
+
+@media screen and (min-width: 770px) {
+  .flow-container > .left {
+  width: calc(8.4rem + (100% - 13.2rem)/12*8);
+}
+.flow-container > .right {
+  width: calc(3.6rem + (100% - 13.2rem)/12*4);
+  padding: 1.2rem;
+}
 }
 </style>
