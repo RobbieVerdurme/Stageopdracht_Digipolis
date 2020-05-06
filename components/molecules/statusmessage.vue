32593fae6af562032d65ce2dd22c51b4069a08f2
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-06 08:35:25 +0200

    [Add] statusmessage component

---
+++ components/molecules/statusmessage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!--Success-->
+    <div v-if="status === 'success'" class="messages messages--status" role="alert" aria-atomic="true">
+      <h2 class="visually-hidden">
+        Status
+      </h2>
+      <i class="icon-checkmark" aria-hidden="true" />
+      <p>{{ statusText }} is opgeslagen</p>
+    </div>
+
+    <!-- Error -->
+    <div v-if="status === 'error'" class="messages messages--error" role="alert" aria-atomic="true">
+      <i class="icon-cross" aria-hidden="true" />
+      <p>{{ statusText }}</p>
+    </div>
+
+    <!-- Warning -->
+    <div v-if="statusText === 'warning'" class="messages messages--warning" role="alert" aria-atomic="true">
+      <i class="icon-exclamation" aria-hidden="true" />
+      <p>{{ statusText }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    statusText: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
