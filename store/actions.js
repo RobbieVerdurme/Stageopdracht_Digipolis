2bd4bd4e5e012f407552b1bddcce38dafd78fa45
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-12 11:12:52 +0200

    [Update] store/actions.js backend poi

--- store/actions.js
+++ store/actions.js
@@ -64,8 +64,29 @@ export default {
   async setPointsOfInterst ({ commit }) {
     // eslint-disable-next-line no-useless-catch
     try {
-      await axios.get('/POI.json', { proxy: { host: process.env.baseUrl, port: process.env.port } }).then(({ data }) => {
-        commit('setPointsOfInterst', data.features)
+      await axios.get('https://licht18qa.stad.gent/jsonapi/node/artwork').then(({ data }) => {
+        const dataFeatures = []
+        for (const index in data.data) {
+          const el = data.data[index]
+          dataFeatures.push({
+            type: 'Feature',
+            id: el.id,
+            geometry: {
+              type: el.attributes.field_coordinates.geo_type,
+              coordinates: [el.attributes.field_coordinates.lon, el.attributes.field_coordinates.lat]
+            },
+            geometry_name: 'SHAPE',
+            properties: {
+              volgnummer: el.attributes.path.pid,
+              naam_nl: el.attributes.title,
+              omschrijving_nl: el.attributes.field_introduction.value,
+              detail_nl: el.attributes.field_main_content.value,
+              link_nl: el.attributes.field_website.uri,
+              symbol: null
+            }
+          })
+        }
+        commit('setPointsOfInterst', dataFeatures)
       })
     } catch (error) {
       throw error
