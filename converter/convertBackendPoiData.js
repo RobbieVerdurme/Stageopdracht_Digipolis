f15189d096cf81173971de473ea8ede553fffa3c
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-14 10:07:53 +0200

    [Update] convertBackendPoiData map function

--- converter/convertBackendPoiData.js
+++ converter/convertBackendPoiData.js
@@ -1,26 +1,21 @@
 export default {
-  convertBackendDataPOI (data) {
-    const dataFeatures = []
-    for (const index in data.data) {
-      const el = data.data[index]
-      dataFeatures.push({
-        type: 'Feature',
-        id: el.id,
-        geometry: {
-          type: el.attributes.field_coordinates.geo_type,
-          coordinates: [el.attributes.field_coordinates.lon, el.attributes.field_coordinates.lat]
-        },
-        geometry_name: 'SHAPE',
-        properties: {
-          volgnummer: el.attributes.field_index,
-          naam_nl: el.attributes.title,
-          omschrijving_nl: el.attributes.field_introduction.value,
-          detail_nl: el.attributes.field_main_content.value,
-          link_nl: el.attributes.field_website.uri,
-          symbol: null
-        }
-      })
-    }
-    return dataFeatures
+  convertBackendDataPOI ({ data }) {
+    return data.map(el => ({
+      type: 'Feature',
+      id: el.id,
+      geometry: {
+        type: el.attributes.field_coordinates.geo_type,
+        coordinates: [el.attributes.field_coordinates.lon, el.attributes.field_coordinates.lat]
+      },
+      geometry_name: 'SHAPE',
+      properties: {
+        volgnummer: el.attributes.field_index,
+        naam_nl: el.attributes.title,
+        omschrijving_nl: el.attributes.field_introduction.value,
+        detail_nl: el.attributes.field_main_content.value,
+        link_nl: el.attributes.field_website.uri,
+        symbol: null
+      }
+    }))
   }
 }
