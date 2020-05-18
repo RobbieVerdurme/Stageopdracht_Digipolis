4c85f7fd268e8b3133c70a32dce84931e41b4d85
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-18 08:37:33 +0200

    [Update] info update styling class

--- components/molecules/info.vue
+++ components/molecules/info.vue
@@ -3,7 +3,9 @@
     <!-- title -->
     <h1>{{ item.naam_nl }}</h1>
     <!--info-->
-    <p><b v-html="item.omschrijving_nl" /></p>
+    <p class="summary-text" v-html="item.omschrijving_nl">
+      { item.omschrijving_nl }}
+    </p>
     <p id="info" v-html="item.detail_nl" />
   </div>
 </template>
