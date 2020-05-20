d834b8619def40a879aba83bfc6754e670cfae58
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-20 08:10:03 +0200

    [Update] workbox-range-request add sync method

--- plugins/workbox-range-request.js
+++ plugins/workbox-range-request.js
@@ -1,19 +1,23 @@
+/* eslint-disable handle-callback-err */
 /* eslint-disable no-undef */
-// import { Queue } from 'workbox-background-sync'
-
-const queue = new workbox.backgroundSync.Queue()
-//  new Queue('myQueueName', {
-//   // onSync: {}, // Function that gets invoked whenever the 'sync' event fires
-//   maxRetentionTime: 24 * 60 // max time to live before removed in the queue IN MINUTES
-// })
+// ADD TO QUEUE
+const queue = new workbox.backgroundSync.Queue('offlineQueue', {
+  maxRetentionTime: 24 * 60 // In MINUTES
+})
 
 self.addEventListener('fetch', (event) => {
   // Clone the request to ensure it's safe to read when
   // adding to the Queue.
   const promiseChain = fetch(event.request.clone()).catch((err) => {
-    console.log(err)
     return queue.pushRequest({ request: event.request })
   })
 
   event.waitUntil(promiseChain)
+})
+
+// SYNC EVENT
+self.addEventListener('sync', function (event) {
+  if (event.tag === 'offlineQueue') {
+    queue.replayRequests()
+  }
 })
