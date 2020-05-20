86694e58631f098208876f73cfbca7c1ba2191e2
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-20 16:02:25 +0200

    [Update] converter add converter for infopoints

--- converter/convertBackendPoiData.js
+++ converter/convertBackendPoiData.js
@@ -13,8 +13,23 @@ export default {
         naam_nl: el.attributes.title,
         omschrijving_nl: el.attributes.field_introduction.value,
         detail_nl: el.attributes.field_main_content.value,
-        link_nl: el.attributes.field_website.uri,
         symbol: null
+      }
+    }))
+  },
+
+  convertBackendDataInfoPOI ({ data }) {
+    return data.map(el => ({
+      type: 'Feature',
+      id: el.id,
+      geometry: {
+        type: el.attributes.field_coordinates.geo_type,
+        coordinates: [el.attributes.field_coordinates.lon, el.attributes.field_coordinates.lat]
+      },
+      geometry_name: 'SHAPE',
+      properties: {
+        volgnummer: el.id,
+        naam_nl: el.attributes.title
       }
     }))
   }
