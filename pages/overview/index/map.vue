f06a0ea9f0732ee42aad412478280ccbe2dbdd89
RobbieVerdurme <robbie.verdurme.y9234@student.hogent.be>
2020-05-20 16:01:50 +0200

    [Update] overview/index/map mapfeatures

--- pages/overview/index/map.vue
+++ pages/overview/index/map.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <client-only>
-      <VueLayersMap :features.sync="features" :route.sync="route" />
+      <VueLayersMap :features.sync="features" :route.sync="route" :info-features="infoFeatures" />
     </client-only>
     <Cta style="margin: 2rem;" />
   </div>
@@ -25,11 +25,19 @@ export default {
       await store.dispatch('setRoutepoints')
       this.route = store.getters.getAllRoutes
     }
+
+    // check if getInfoPointsOfInterst in store is filled
+    if (!store.getters.getInfoPointsOfInterst.length) {
+    // get getInfoPointsOfInterst from online
+      await store.dispatch('setInfoPointsOfInterst')
+      this.infoFeatures = store.getters.getInfoPointsOfInterst
+    }
   },
   data () {
     return {
       features: this.$store.getters.getAllPointsOfIntrest,
-      route: this.$store.getters.getAllRoutes
+      route: this.$store.getters.getAllRoutes,
+      infoFeatures: this.$store.getters.getInfoPointsOfInterst
     }
   }
 }
